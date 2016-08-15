f0bceecb4521e7f2596fa1dfd627a4d1c790a14a
Sebastian Schmidt <publicarray@icloud.com>
2016-08-15 14:55:19 +1000

    fix trailing

--- 02TrimStr/trailing.c
+++ 02TrimStr/trailing.c
@@ -1,17 +1,14 @@
 #include <strings.h>
+// #include "include.h"
+
 //-- Removes trailing blanks from s --
-void trailing(char *s)
-{
+void trailing(char *s) {
     // int index = MAXLEN;
     int slength = strlen(s);
-    for (int i = 0; i < strlen(s); i++) {
-        if (s[i] == ' ' || s[i] == '\t') {
-            slength--;
-        }
+
+    while ((s[slength] == ' ' || s[slength] == '\t' || s[slength] == '\0')) {
+        slength--;
     }
-    // while ((s[slength] == ' ' || s[slength] == '\t')) {
-    //     --slength;
-    // }
 
     s[slength] = '\0';
 }
