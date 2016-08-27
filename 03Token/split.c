5bcfcf9281a0cecdd90f7f8dcb97fbd03984ec8d
Sebastian Schmidt <publicarray@icloud.com>
2016-08-27 10:30:03 +1000

    ignore whitespace
    Signed-off-by: Sebastian Schmidt <publicarray@icloud.com>

--- 03Token/split.c
+++ 03Token/split.c
@@ -10,31 +10,34 @@ int strLength(char *string) {
 
 int ParseLine(char* srcString, char* tokens[], int maxTokens)
 {
-    int numFound = -1;
+    int numFound = 0;
+    int saved = 0;
     int length = strLength(srcString);
-
-    tokens[0] = &srcString[0];
-    numFound++;
 
     for (int i = 0; i < length; i++) { // search each char
         if (numFound >= maxTokens) {
             printf("Err: Hit max number of token limit, I need more space!\n");
             return numFound;
         }
+
         if (srcString[i] == ' ') { // if delimiter is found
             srcString[i] = 0x00; // replace with null char
-            tokens[numFound+1] = &srcString[i+1]; // save the address
+            saved = 0; // marked as not saved (need to save on next iteration)
+        } else if (saved == 0) { // if not saved
+            tokens[numFound] = &srcString[i]; // save the address
             numFound++;
+            saved = 1; // ignore (don't save anything)
+        } else { // all other characters
+            saved = 1; // ignore (don't save anything)
         }
     }
     return numFound;
 }
 
-
 int main(int argc, char const *argv[])
 {
     int numTokensToSearchFor = 50;
-    char string[] = "Hello World!    My Name is Foo.";
+    char string[] = "Hello World!        My Name is Foo.";
 
     char* tokens[numTokensToSearchFor];
     int tokensFound = ParseLine(string, tokens, numTokensToSearchFor);
