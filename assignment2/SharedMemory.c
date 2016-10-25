13f93e733855c48e119e4ce7fe6edbce5b63d16f
Sebastian Schmidt <publicarray@icloud.com>
2016-10-25 13:35:13 +1000

    update shared memory

--- assignment2/SharedMemory.c
+++ assignment2/SharedMemory.c
@@ -7,28 +7,52 @@ static void __freeSharedMemory(SharedMemory *self) {
     if (shmctl(self->id, IPC_RMID, NULL) == -1) {
         perror("shmctl");
     }
+    self->id = 0;
+    self->size = 0;
+    self->data = NULL;
+    self->data32 = NULL;
+    self->dataInt = NULL;
 }
 
-static void __updatePointerSharedMemory(SharedMemory *self) {
-    self->data = shmat(self->id, NULL, 0);
+static void __attachPointerSharedMemory(SharedMemory *self) {
+
+    // SHM_RDONLY = read only
+    // SHM_RND = round down to SHMLBA. Otherwise shmaddr must be a page-aligned address at which the attach occurs (1024, pagesize = 4096).
+    // For the current implementation the SHMLBA value is PAGE_SIZE
+
+    self->data = shmat(self->id, NULL, SHM_RND);
+    if (shmdt(self->data) == -1) {
+        fprintf(stderr, "failed to attach\n");
+        perror("shmat");
+        exit(-1);
+    }
+    self->data32 = self->data;
+    self->dataInt = self->data;
 }
 
-static uint32_t * __getAsNumSharedMemory(SharedMemory *self) {
-    return (uint32_t *)(self->data);
+static void __print32SharedMemory(SharedMemory *self) {
+    for (int i = 0; i < self->size; ++i) {
+        printf("%p -> %d\n", self->data32+i, self->data32[i]);
+        printf("%p\n", self->data32+i);
+    }
 }
 
 SharedMemory newSharedMemory(size_t size) {
     SharedMemory shm;
+    shm.size = size;
+    shm.data = NULL; // need to initialise manually, using attach
+    shm.data32 = NULL; // need to initialise manually, using attach
+    shm.dataInt = NULL; // need to initialise manually, using attach
     shm.id = shmget(IPC_PRIVATE, size, IPC_CREAT | 0666);
-    if (shm.id == -1) {
+    if (shm.id < 0) {
+        fprintf(stderr, "failed to create shared memory segment\n");
         perror("shmget");
-        exit(1);
+        exit(-1);
     }
-    __updatePointerSharedMemory(&shm);
 
     shm.free = __freeSharedMemory;
-    shm.update = __updatePointerSharedMemory;
-    shm.getNum = __getAsNumSharedMemory;
+    shm.attach = __attachPointerSharedMemory;
+    shm.print = __print32SharedMemory;
 
     return shm;
 }
--- assignment2/SharedMemory.h
+++ assignment2/SharedMemory.h
@@ -6,32 +6,17 @@
 typedef struct SharedMemory_
 {
     int id;
+    unsigned int size;
     void *data;
+    int *dataInt;
+    uint32_t *data32;
 
     void (*free)(struct SharedMemory_ *self);
-    void (*update)(struct SharedMemory_ *self);
-    uint32_t * (*getNumP)(struct SharedMemory_ *self);
+    void (*attach)(struct SharedMemory_ *self);
+    void (*print)(struct SharedMemory_ *self);
 
 } SharedMemory;
 
 SharedMemory newSharedMemory(size_t size);
 
 #endif
-
-
-// #ifndef THREAD_H
-// #define THREAD_H
-
-// #include <pthread.h>
-
-// typedef struct Thread_
-// {
-//     pthread_t id;
-//     void (*startDetached)(struct Thread_ * self, void* (*f) (void *), void * data); //start thread detatched (don't call join)
-//     void (*start)(struct Thread_ * self, void* (*f) (void *), void * data); //start regular thread (call join to get return value and clean up memory)
-//     void * (*join)(struct Thread_ * self); //wait for thread to finish and get the return value
-// } Thread;
-
-// Thread newThread();
-
-// #endif
