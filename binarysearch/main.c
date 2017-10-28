43dd1c892cf6968771824a68d4e638af127c921e
Sebastian Schmidt <publicarray@icloud.com>
2017-10-28 23:31:18 +1000

    Add number guessing game

---
+++ binarysearch/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <math.h>
+
+// Choose a number from 1 to 100(maxNum)
+// the aim is to guess the correct number in 7 gesses
+// I will tell you if you are too high or to low
+
+int answer = 55; // Your chosen number
+int maxNum = 100;
+int verbose = 1;
+
+int guessTheAnswer(int guess);
+void comparisonLoopSearch ();
+void binarySearch ();
+int mylog2(int num);
+
+int main (int argc, char const *argv[]) {
+    // ToDo: get awnser and maxNum from commandline arguments
+    comparisonLoopSearch(maxNum);
+    binarySearch(maxNum);
+    return 0;
+}
+
+// -1 toHigh
+// 0 correct answer
+// 1 toLow
+int guessTheAnswer(int guess) {
+    if (answer < guess) {
+        return -1;
+    } else if (answer > guess) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void comparisonLoopSearch () {
+    for (int guess = 0; guess <= maxNum; guess++ ) {
+        if (guessTheAnswer(guess) == 0) {
+            printf("comparisonLoopSearch - found answer: %d in %d guesses\n", guess, guess);
+        }
+    }
+}
+
+void binarySearch () {
+    int guesses = 0;
+    int midPoint = maxNum/2;
+    int upperBound = maxNum+1;
+    int lowerBound = 0;
+
+    int maxGuesses = log2(maxNum);
+    printf("binarySearch - maxGuesses: %d\n", maxGuesses);
+    while (guesses <= maxGuesses) { //Logorithmic time -> O log (n)
+        if (verbose == 1) {
+            printf("binarySearch - guess: %d\n", midPoint);
+        }
+
+        guesses++;
+        int test = guessTheAnswer(midPoint);
+        if (test == -1) { // -1 toHigh
+            upperBound = midPoint;
+        } else if (test == 1 ) { // 1 toLow
+            lowerBound = midPoint;
+        } else {
+            printf("binarySearch - found answer: %d in %d guesses\n", midPoint, guesses);
+            return;
+        }
+        midPoint = lowerBound + (upperBound - lowerBound) / 2;
+    }
+}
