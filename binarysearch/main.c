60f4371f8754bf7a1495efc2aa0b58b3f5e501f0
Sebastian Schmidt <publicarray@icloud.com>
2017-10-29 08:28:19 +1000

    binarysearch: Use command line parameters

--- binarysearch/main.c
+++ binarysearch/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
+#include <string.h>
 
 // Choose a number from 1 to 100(maxNum)
 // the aim is to guess the correct number in 7 guesses
@@ -9,13 +12,38 @@ int answer = 55; // Your chosen number
 int maxNum = 100;
 int verbose = 1;
 
-int guessTheAnswer(int guess);
+int guessTheAnswer (int guess);
 void comparisonLoopSearch ();
 void binarySearch ();
-int mylog2(int num);
+int isStrANumber (const char * str);
+
+int isStrANumber (const char * str) {
+    int length = strlen (str);
+    for(int i = 0; i < length; i++) {
+        if (!isdigit(str[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 int main (int argc, char const *argv[]) {
-    // ToDo: get awnser and maxNum from commandline arguments
+
+    if (argc > 1) {
+        if (!isStrANumber(argv[1])) {
+            printf ("Error: '%s' is not a number.\n", argv[1]);
+            exit(1);
+        }
+        answer = atoi(argv[1]);
+    }
+    if (argc > 2) {
+        if (!isStrANumber(argv[2])) {
+            printf ("Error: '%s' is not a number.\n", argv[2]);
+            exit(1);
+        }
+        maxNum = atoi(argv[2]);
+    }
+
     comparisonLoopSearch(maxNum);
     binarySearch(maxNum);
     return 0;
@@ -24,7 +52,7 @@ int main (int argc, char const *argv[]) {
 // -1 tooHigh
 // 0 correct answer
 // 1 tooLow
-int guessTheAnswer(int guess) {
+int guessTheAnswer (int guess) {
     if (answer < guess) {
         return -1;
     } else if (answer > guess) {
